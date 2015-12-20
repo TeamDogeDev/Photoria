0a3ef2f775cf351394cd17fde8b313b2ae49002e
elektropapst <christopher.brumann@googlemail.com>
2015-12-20 14:09:22 +0100

    increased resolution

--- desktop/src/de/dogedevs/photoria/desktop/DesktopLauncher.java
+++ desktop/src/de/dogedevs/photoria/desktop/DesktopLauncher.java
@@ -5,8 +5,10 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import de.dogedevs.photoria.MainGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MainGame(), config);
-	}
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 1280;
+        config.height = 720;
+        new LwjglApplication(new MainGame(), config);
+    }
 }
