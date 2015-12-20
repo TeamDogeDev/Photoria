3d09869526c485029c3ffd92ed310c3f9bd2146b
Christopher Brumann <christopher.brumann@googlemail.com>
2015-12-20 12:17:41 +0100

    added basic models

---
+++ core/src/de/dogedevs/photoria/model/Entity.java
@@ -0,0 +1,8 @@
+package de.dogedevs.photoria.model;
+
+/**
+ * Created by elektropapst on 20.12.2015.
+ */
+public abstract class Entity {
+
+}
---
+++ core/src/de/dogedevs/photoria/model/Mob.java
@@ -0,0 +1,7 @@
+package de.dogedevs.photoria.model;
+
+/**
+ * Created by elektropapst on 20.12.2015.
+ */
+public class Mob extends Entity {
+}
