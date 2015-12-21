64f2dd9f122779f48020a31d07c05d82652c7e00
elektropapst <christopher.brumann@googlemail.com>
2015-12-21 17:49:01 +0100

    fixes

--- core/src/de/dogedevs/photoria/model/ChunkBuffer.java
+++ core/src/de/dogedevs/photoria/model/ChunkBuffer.java
@@ -20,6 +20,39 @@ public class ChunkBuffer {
         decorator = new MapDecorator();
     }
 
+    private int[][] createGroundLayer(Chunk chunk, int size, AbstractMapGenerator generator) {
+        int[][] generatedMap = generator.generate(chunk.x, chunk.y, 64);
+
+        ChunkCell cell;
+        chunk.addLayer(1, new ChunkCell[64][64]);
+        for (int row = 0; row < 32 << 1; row++) {
+            for (int col = 0; col < 32 << 1; col++) {
+                cell = new ChunkCell();
+                cell.value = generatedMap[row][col];
+                if(generator instanceof ChunkDebugMapGenerator){
+                    chunk.setCell(cell, row, col, 1);
+                    continue;
+                }
+                chunk.setCell(cell, row, col, 1);
+            }
+        }
+        return generatedMap;
+    }
+
+    private void createDecoration(Chunk chunk, int[][] generatedMap) {
+        generatedMap = decorator.decorate(generatedMap);
+
+        ChunkCell cell;
+        chunk.addLayer(2, new ChunkCell[64][64]);
+        for (int row = 0; row < 32 << 1; row++) {
+            for (int col = 0; col < 32 << 1; col++) {
+                cell = new ChunkCell();
+                cell.value = generatedMap[row][col];
+                chunk.setCell(cell, row, col, 2);
+            }
+        }
+    }
+
     public ChunkCell getCell(int x, int y, int layer) {
 
         Chunk chunk = chunks.get((x/64)+"_"+(y/64));
@@ -42,36 +75,9 @@ public class ChunkBuffer {
 
             chunks.put(chunk.getHashCode(), chunk);
 
-            int[][] generatedMap = generator.generate(chunk.x, chunk.y, 64);
+            int[][] groundLayer = createGroundLayer(chunk, 64, generator);
+            createDecoration(chunk, groundLayer);
 
-            ChunkCell cell;
-            chunk.addLayer(1, new ChunkCell[64][64]);
-            for (int row = 0; row < 32 << 1; row++) {
-                for (int col = 0; col < 32 << 1; col++) {
-                    cell = new ChunkCell();
-                    cell.value = generatedMap[row][col];
-                    if(generator instanceof ChunkDebugMapGenerator){
-                        chunk.setCell(cell, row, col, 1);
-                        continue;
-                    }
-                    chunk.setCell(cell, row, col, 1);
-                }
-            }
-
-            generatedMap = decorator.decorate(generatedMap);
-
-            chunk.addLayer(2, new ChunkCell[64][64]);
-            for (int row = 0; row < 32 << 1; row++) {
-                for (int col = 0; col < 32 << 1; col++) {
-                    cell = new ChunkCell();
-                    cell.value = generatedMap[row][col];
-                    if(generator instanceof ChunkDebugMapGenerator){
-                        chunk.setCell(cell, row, col, 2);
-                        continue;
-                    }
-                    chunk.setCell(cell, row, col, 2);
-                }
-            }
 
         }
 
