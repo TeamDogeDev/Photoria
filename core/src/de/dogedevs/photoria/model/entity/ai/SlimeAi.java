13909d2420765d462734e2290e057b63c8b23e50
elektropapst <christopher.brumann@googlemail.com>
2016-01-03 17:25:11 +0100

    more ai

--- core/src/de/dogedevs/photoria/model/entity/ai/SlimeAi.java
+++ core/src/de/dogedevs/photoria/model/entity/ai/SlimeAi.java
@@ -2,6 +2,7 @@ package de.dogedevs.photoria.model.entity.ai;
 
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
+import com.badlogic.gdx.math.MathUtils;
 import de.dogedevs.photoria.model.entity.ComponentMappers;
 import de.dogedevs.photoria.model.entity.components.AiComponent;
 import de.dogedevs.photoria.model.entity.components.PlayerComponent;
@@ -15,6 +16,7 @@ import de.dogedevs.photoria.screens.GameScreen;
 public class SlimeAi implements AiComponent.AiInterface {
 
     private Entity playerEntity;
+    private static final float DIST = 200;
 
     public SlimeAi() {
         playerEntity = GameScreen.getAshley().getEntitiesFor(Family.all(PlayerComponent.class).get()).get(0);
@@ -28,13 +30,35 @@ public class SlimeAi implements AiComponent.AiInterface {
 
         VelocityComponent velocity = ComponentMappers.velocity.get(self);
         PositionComponent selfPosition = ComponentMappers.position.get(self);
+        if(velocity != null && selfPosition != null) {
+            double eDist = Math.sqrt(Math.pow(playerPosition.x - selfPosition.x, 2) +
+                    Math.pow(playerPosition.y - selfPosition.y, 2));
 
-        double sqrt = Math.sqrt(Math.pow(playerPosition.x - selfPosition.x, 2) +
-                Math.pow(playerPosition.y - selfPosition.y, 2));
-        if(sqrt <= 100) {
-            velocity.speed = 20;
-        } else {
-            velocity.speed = 0;
+            if (eDist <= DIST) {
+                if (playerPosition.x - selfPosition.x > 0) {
+                    if (playerPosition.y - selfPosition.y > 0) {
+                        velocity.direction = VelocityComponent.NORTH_EAST;
+                    } else {
+                        velocity.direction = VelocityComponent.SOUTH_EAST;
+                    }
+                } else {
+                    if (playerPosition.y - selfPosition.y > 0) {
+                        velocity.direction = VelocityComponent.NORTH_WEST;
+                    } else {
+                        velocity.direction = VelocityComponent.SOUTH_WEST;
+                    }
+                }
+                velocity.speed = 80;
+            } else {
+                if(MathUtils.randomBoolean(0.001f)){
+                    velocity.direction = MathUtils.random(0, 7);
+                }
+                if(velocity.blockedDelta > 0.5f){
+                    velocity.direction = MathUtils.random(0, 7);
+                    velocity.blockedDelta = 0;
+                }
+                velocity.speed = 20;
+            }
         }
     }
 
