2bd62f2b9da3a95162188bd645853ec8f8617356
meisterfuu <info@meister-fuu.de>
2016-01-03 12:26:30 +0100

    Fixed missing 8 direction movement rendering

--- core/src/de/dogedevs/photoria/model/entity/ai/DefaultMovingAi.java
+++ core/src/de/dogedevs/photoria/model/entity/ai/DefaultMovingAi.java
@@ -18,10 +18,10 @@ public class DefaultMovingAi implements AiComponent.AiInterface {
 
         if(velocity != null){
             if(MathUtils.randomBoolean(0.001f)){
-                velocity.direction = MathUtils.random(0, 3);
+                velocity.direction = MathUtils.random(0, 7);
             }
             if(velocity.blockedDelta > 0.5f){
-                velocity.direction = MathUtils.random(0, 3);
+                velocity.direction = MathUtils.random(0, 7);
                 velocity.blockedDelta = 0;
             }
         }
--- core/src/de/dogedevs/photoria/model/entity/systems/EntityDrawSystem.java
+++ core/src/de/dogedevs/photoria/model/entity/systems/EntityDrawSystem.java
@@ -106,6 +106,18 @@ public class EntityDrawSystem extends EntitySystem implements EntityListener {
                             case VelocityComponent.EAST:
                                 batch.draw(animation.rightAnimation.getKeyFrame(animation.stateTime, true), position.x-animation.idleAnimation.getKeyFrames()[0].getRegionWidth()/2, position.y);
                                 break;
+                            case VelocityComponent.SOUTH_WEST:
+                                batch.draw(animation.downAnimation.getKeyFrame(animation.stateTime, true), position.x-animation.idleAnimation.getKeyFrames()[0].getRegionWidth()/2, position.y);
+                                break;
+                            case VelocityComponent.SOUTH_EAST:
+                                batch.draw(animation.downAnimation.getKeyFrame(animation.stateTime, true), position.x-animation.idleAnimation.getKeyFrames()[0].getRegionWidth()/2, position.y);
+                                break;
+                            case VelocityComponent.NORTH_WEST:
+                                batch.draw(animation.upAnimation.getKeyFrame(animation.stateTime, true), position.x-animation.idleAnimation.getKeyFrames()[0].getRegionWidth()/2, position.y);
+                                break;
+                            case VelocityComponent.NORTH_EAST:
+                                batch.draw(animation.upAnimation.getKeyFrame(animation.stateTime, true), position.x-animation.idleAnimation.getKeyFrames()[0].getRegionWidth()/2, position.y);
+                                break;
                         }
                     }
                 } else {
