40091f9bedc9c8fac99c39d3653635147a28d984
meisterfuu <info@meister-fuu.de>
2016-01-17 00:24:00 +0100

    Bloated ItemComponent

--- core/src/de/dogedevs/photoria/model/entity/components/ItemComponent.java
+++ core/src/de/dogedevs/photoria/model/entity/components/ItemComponent.java
@@ -11,6 +11,22 @@ public class ItemComponent implements Component, Pool.Poolable {
     public String name;
     public ItemType type;
 
+    public float maxLife;
+    public float maxEnergy;
+    public float lifeReg;
+    public float energyReg;
+    public float dmgElementPurple;
+    public float dmgElementGreen;
+    public float dmgElementRed;
+    public float dmgElementBlue;
+    public float dmgElementYellow;
+    public float defElementPurple;
+    public float defElementGreen;
+    public float defElementRed;
+    public float defElementBlue;
+    public float defElementYellow;
+    public float movementSpeed;
+
     public ItemComponent() {
         reset();
     }
@@ -19,6 +35,21 @@ public class ItemComponent implements Component, Pool.Poolable {
     public void reset() {
         name = "Item";
         type = ItemType.OTHER;
+        maxLife = 0;
+        maxEnergy = 0;
+        lifeReg = 0;
+        energyReg = 0;
+        dmgElementPurple = 0;
+        dmgElementGreen = 0;
+        dmgElementRed = 0;
+        dmgElementBlue = 0;
+        dmgElementYellow = 0;
+        defElementPurple = 0;
+        defElementGreen = 0;
+        defElementRed = 0;
+        defElementBlue = 0;
+        defElementYellow = 0;
+        movementSpeed = 0;
     }
 
     public enum ItemType {
